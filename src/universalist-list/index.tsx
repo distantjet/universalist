7b24cf19cb2286b297b4779990e5de3ca5a99713
Matias Escudero <matiasescudero@matias.local>
2026-02-20 00:46:31 -0300

    fixed edit.tsx persistence

--- src/universalist-list/index.tsx
+++ src/universalist-list/index.tsx
@@ -1,13 +1,12 @@
 import { registerBlockType, BlockConfiguration } from '@wordpress/blocks';
+import { InnerBlocks } from '@wordpress/block-editor'; // Add this import
 import Edit from './edit';
 import metadata from './block.json';
 import DistantJet_UniversalistIcon from "../components/icon";
 
-
-// Cast metadata to any or BlockConfiguration to satisfy the registerBlockType signature
 registerBlockType(metadata.name as string, {
     ...metadata,
     edit: Edit,
     icon: DistantJet_UniversalistIcon,
-    save: () => null,
+    save: () => <InnerBlocks.Content />, 
 } as BlockConfiguration);
