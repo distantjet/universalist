9ba099dd6370a01f3c7f4c54b429b270a7789604
Matias Escudero <matiasescudero@matias.local>
2026-02-19 21:20:25 -0300

    about to fix document-panel warnings

--- src/universalist-page-title/index.tsx
+++ src/universalist-page-title/index.tsx
@@ -1,10 +1,11 @@
 import './document-panel';
 import DistantJet_UniversalistIcon from "../components/icon";
-import { registerBlockType } from '@wordpress/blocks';
-import metadata from './block.json';
+import { registerBlockType, BlockConfiguration } from '@wordpress/blocks';
+import metadataJson from './block.json';
 
+const metadata = (metadataJson as unknown) as BlockConfiguration;
 
-registerBlockType(metadata.name, {
+registerBlockType(metadata, {
     icon: DistantJet_UniversalistIcon,
     edit: () => null,
     save: () => null,
